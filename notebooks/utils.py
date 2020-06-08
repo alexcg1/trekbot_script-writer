49a7f3b30fbe3a6808056757af28b3b748c9c792
Alex C-G <alexcg@outlook.com>
2020-06-08 16:04:21 +1200

    Utils and requirements

---
+++ notebooks/utils.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+class Setup:
+    def dir_setup(dir_list):
+        for directory in dir_list:
+            if not os.path.isdir(directory):
+                os.mkdir(directory)
+                print(f"Created {directory}")
+            else:
+                print(f"Skipped {directory}. It already exists")
+                
+class Diag:
+    import csv
+    import os
+
+    def setup(filename):
+#         logwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#         pass
+        
+        loss_log = os.path.join("/", "kaggle", "working", "diagnostics", filename)
+        log_headers = ["Time", "Learning Rate", "Loss"]
+        
+        if not os.path.exists(loss_log):
+            print(f"Creating new log at {loss_log}")
+            with open(loss_log, 'w', newline='') as csvfile:
+                  logwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        else:
+            print(f"Using existing log")
