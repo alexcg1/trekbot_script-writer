7dfa47edbe4406d6d3cd8676915c8266615d3160
Alex C-G <alexcg@outlook.com>
2020-06-09 12:13:26 +1200

    Add logging

--- utils.py
+++ utils.py
@@ -25,5 +25,18 @@ class Diag:
             print(f"Creating new log at {loss_log}")
             with open(loss_log, 'w', newline='') as csvfile:
                   logwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                  logwriter.writerow(log_headers)
         else:
             print(f"Using existing log")
+
+    def write_row(data, file_path):
+        """
+        @param: data: list of data you want written to a row, one datum per column
+        @param: file_path: path to the CSV file you want to write to
+        """
+        if not os.path.exists(file_path):
+            raise IOError(f"File {file_path} not found")
+        else:
+            with open(file_path, 'a', newline='') as csvfile:
+                  logwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                  logwriter.writerow(data)
